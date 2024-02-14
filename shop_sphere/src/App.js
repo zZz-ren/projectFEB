fa132a79bc03c6ac1695038cfb8a1419128bdc99
THAKKAR DHRUTI <thakkardhruti22@gmail.com>
2024-02-14 23:01:03 +0530

    on another branch

--- shop_sphere/src/App.js
+++ shop_sphere/src/App.js
@@ -1,12 +1,11 @@
 import LandingPage from "./Components/LandingPage";
 
 function App() {
-  return <div className="w-screen h-[calc(100vh)] ">
-    <LandingPage />
-  </div>;
-
+  return (
+    <div className="w-screen h-[calc(100vh)] ">
+      <LandingPage />
+    </div>
+  );
 }
-
-
 
 export default App;
