cc2df77666eba4156dd81ba7ad5cdf65f565275e
THAKKAR DHRUTI <thakkardhruti22@gmail.com>
2024-02-23 11:34:43 +0530

    commit

--- shop_sphere/src/App.js
+++ shop_sphere/src/App.js
@@ -2,42 +2,40 @@ import CardContainer from "./Components/Card";
 import LandingPage from "./Components/LandingPage";
 import Layout from "./Pages/Layout";
 import ProductDetails from "./Pages/ProductDetails";
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 function App() {
-
   const router = createBrowserRouter([
     {
-      path: '/',
-      element: <LandingPage />
+      path: "/",
+      element: <LandingPage />,
     },
     {
       element: <Layout />,
       children: [
         {
-          path: '/home',
-          element: <div>Pending</div>
+          path: "/home",
+          element: <div>Pending</div>,
         },
         {
-          path: '/products',
-          element: <CardContainer />
+          path: "/products",
+          element: <CardContainer />,
         },
         {
-          path: '/products/:id',
-          element: <ProductDetails />
-        }
-      ]
-    }
-  ])
+          path: "/products/:id",
+          element: <ProductDetails />,
+        },
+      ],
+    },
+  ]);
 
-  return <div className=" ">
-
-    <RouterProvider router={router} />
-    {/* <Products /> */}
-    {/* <LandingPage /> */}
-
-  </div>;
-
+  return (
+    <div className=" ">
+      <RouterProvider router={router} />
+      {/* <Products /> */}
+      {/* <LandingPage /> */}
+    </div>
+  );
 }
 
 export default App;
